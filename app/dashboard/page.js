5549c945a1aae072b37efde21128a393202eeb11
JasmineBabaYara <babayarajasmine@gmail.com>
2024-04-05 18:31:10 +0000

    add dashboard

---
+++ app/dashboard/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="mt-4 h-screen bg-white">
+      <div className="grid grid-cols-4">
+        <div className="bg-secondary h-40 rounded-xl col-span-2"></div>
+      </div>
+      <div className="grid grid-cols-3 mt-4">
+        <div className="bg-secondary h-40 rounded-xl col-span-2"></div>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
 import Nav from "./navbar/page";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
-   <Nav />
-  )
+    <div className="bg-white pl-6 pr-6">
+      <Nav />
+      <Dashboard />
+    </div>
+  );
 }
