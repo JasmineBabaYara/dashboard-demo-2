566a1b229209c3a3365ed51ab507c1bbe0efca00
JasmineBabaYara <babayarajasmine@gmail.com>
2024-04-06 15:41:47 +0000

    change opacity of secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
       colors: {
         primary:'#2244F6',
-        secondary:'rgba(34, 68, 246, 0.1)',
+        secondary:'rgba(34, 68, 246, 0.05)',
         gray:'#A19B9B',
       },
     },
