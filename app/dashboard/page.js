0179b36f3b4c96a6b63af2bc42a58bb94a3aac06
JasmineBabaYara <babayarajasmine@gmail.com>
2024-04-10 15:58:20 +0000

    add donut chart section

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,8 @@
 "use client";
 import React from "react";
 import { IoIosArrowDown } from "react-icons/io";
+import { BsThreeDots } from "react-icons/bs";
+import { CiCreditCard1 } from "react-icons/ci";
 import BarChart from "../barchart/page";
 import DonutChart from "../donutchart/page";
 
@@ -19,24 +21,29 @@ export default function page() {
           <BarChart />
         </div>
         <div className="bg-red-50 rounded-xl col-span-1 p-4 h-96">
-        <div className="flex justify-between items-center">
-            <p className="font-medium text-black text-base">Available Revenue</p>
-              <IoIosArrowDown />
+          <div className="flex justify-between items-center">
+            <p className="font-medium text-black text-base">
+              Available Revenue
+            </p>
+            <BsThreeDots />
           </div>
           <DonutChart />
-          <div className="flex mt-2">
+          <div className="flex mt-2 gap-x-8">
             <div>
-          <p className="font-medium text-black text-sm">25%</p>
-          <p className="text-black text-xs">Available</p>
-          <p className="text-black text-base">Available Revenue</p>
+              <p className="text-black font-bold text-sm">25%</p>
+              <p className="text-black text-xs">Available</p>
+              <p className="h-2 bg-white w-32 rounded-lg mt-1">
+                <p className="h-2 w-10 bg-red-600 rounded-lg" />
+              </p>
+            </div>
+            <div>
+              <p className="text-black font-bold text-sm">75%</p>
+              <p className="text-black text-xs">Unavailable</p>
+              <p className="h-2 bg-white w-32 rounded-lg mt-1">
+                <p className="h-2 w-20 bg-[#ffcac8] rounded-lg" />
+              </p>
+            </div>
           </div>
-          <div>
-          <p className="font-medium text-black text-sm">75%</p>
-          <p className="text-black text-xs">Unavailable</p>
-          <p className="text-black text-base">Available Revenue</p>
-          </div>
-          </div>
-
         </div>
       </div>
       <div className="grid grid-cols-3 mt-4">
